b79984afe5a8ff003cda248bbf6bc459ee3f152b
Qusli <quali123@list.ru>
2025-02-01 17:14:54 +0300

    update: logger update

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,11 @@
+import os;
 import logging;
 
 class Logger:
     def __init__(self):
+        if (not(os.path.exists("logs"))): 
+            os.mkdir("logs")
+
         logging.basicConfig(filename="logs/app.log", level=logging.INFO, encoding="utf-8")
         logging.basicConfig(filename="logs/app.error.log", level=logging.ERROR, encoding="utf-8")
 
